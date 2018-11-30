47e675cc03e74c164806c0be25c53eb1a5f5c076
Raghu Betina <raghu@firstdraft.com>
2018-11-30 13:39:51 +0000

    Whitelist columns in Result for ActiveAdmin

--- app/admin/results.rb
+++ app/admin/results.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Result do
+
+ permit_params :user_id, :race_id, :time, :overall_place_id, :age_group_place_id, :body
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
